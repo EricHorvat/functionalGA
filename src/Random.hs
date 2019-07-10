50056c01ca6c6a7f8e5c79d32eddc46df55908ce
Eric Horvat <eric.nahuel.horvat@gmail.com>
2019-07-10 16:59:17 -0300

    [TYPO] Almost all StdGen -> Seed

--- src/Random.hs
+++ src/Random.hs
@@ -12,23 +12,23 @@ import System.Random
 
 type Seed = StdGen
 
-randSeeds :: StdGen -> Int -> [StdGen]
+randSeeds :: Seed -> Int -> [Seed]
 randSeeds g i = snd (foldr (\x (y,z) -> (snd (split y), fst (split y) : z)) (g,[]) [1..i])
 
-randInt :: StdGen -> Int
+randInt :: Seed -> Int
 randInt g = fst (random g)
 
-randBoundedInt :: StdGen -> Int -> Int -> Int
+randBoundedInt :: Seed -> Int -> Int -> Int
 randBoundedInt g min max = fst (randomR (min,max) g)
 
-randDouble :: StdGen -> Double
+randDouble :: Seed -> Double
 randDouble g = fst (random g)
 
-randList :: Random a => StdGen -> Int -> ([a], StdGen)
+randList :: Random a => Seed -> Int -> ([a], Seed)
 randList g i = foldr (\x (y,z) -> (fst (random z) : y, snd (next z))) ([fst (random g)], snd (next g)) [1..i]
 
-randInts :: StdGen -> Int -> ([Int], StdGen)
+randInts :: Seed -> Int -> ([Int], Seed)
 randInts = randList
 
-randDoubles :: StdGen -> Int -> ([Double], StdGen)
+randDoubles :: Seed -> Int -> ([Double], Seed)
 randDoubles = randList
