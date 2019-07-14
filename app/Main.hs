0c6c3202d6d90d372ea437acd4ea7854da165c5b
Eric Horvat <eric.nahuel.horvat@gmail.com>
2019-07-13 23:09:22 -0300

    [ADD] Polinomial root moving coefficients fitness done

--- app/Main.hs
+++ app/Main.hs
@@ -11,20 +11,30 @@ import Random
 import System.Random
 import Control.Monad.Trans.State
 
-sumFitness:: Chromosome -> Double
+sumFitness :: FitnessFunction
 sumFitness chr = sum (map alleleValue chr)
+
+polynomialRootFitness :: Double -> FitnessFunction
+polynomialRootFitness value chr = 1 / (1 + abs (sum (
+  zipWith (*)
+    (zipWith (**)
+      (map (const value) [1..length chr])
+      (map fromIntegral [0..(length chr - 1)]))
+    (map alleleValue chr)
+  )))
 
 main :: IO ()
 main = do
   stdGen <- newStdGen
+  let fitnessF = polynomialRootFitness 6
   let seedPop = initialPopulation (boundedIntChromosomeGenerator 5 (-150,150)) stdGen 50
 
   let (s1:s2:s3:s4:sEnd:_) = randSeeds (snd seedPop) 5
-  let resultPop = ga 500 (const False) (nextGen eliteSelection 4 cross1point 0.2 mutateMultiGenChromosome 0.1 replaceOld eliteSelection sumFitness) seedPop
+  let resultPop = ga 500 (const False) (nextGen eliteSelection 4 cross1point 0.2 mutateMultiGenChromosome 0.1 replaceOld eliteSelection fitnessF) seedPop
   print resultPop
 
-  print (randomSelection resultPop 25 sumFitness sEnd)
-  print (rouletteSelection resultPop 25 sumFitness sEnd)
+  print (randomSelection resultPop 25 fitnessF sEnd)
+  print (rouletteSelection resultPop 25 fitnessF sEnd)
 
   print (randSeeds s1 5)
   --print (evalState allTypes stdGen)
